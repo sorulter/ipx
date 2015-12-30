6bff57b2f182a5c053bb661a0da0a2c0ae02e85b
Frank Kung <kfanjian@gmail.com>
2015-12-31 00:39:36 +0800

    Add main file.

---
+++ src/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var (
+	err error
+)
+
+func main() {
+
+	// Exit main loop
+	sig := make(chan os.Signal)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+forever:
+
+	for {
+		select {
+		case s := <-sig:
+			fmt.Printf("\nSignal (%d) received, stopping\n", s)
+			break forever
+		}
+	}
+
+}
