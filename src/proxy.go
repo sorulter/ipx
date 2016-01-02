7f6947a83a2d42882f4aeb1c1f662e47b26d4cc3
Frank Kung <kfanjian@gmail.com>
2016-01-02 18:08:42 +0800

    Remove proxy init code.

--- src/proxy.go
+++ src/proxy.go
@@ -21,31 +21,7 @@ type ProxydManager struct {
 }
 
 var (
-	proxy *goproxy.ProxyHttpServer
 )
-
-func init() {
-	proxy = goproxy.NewProxyHttpServer()
-	proxy.Verbose = true
-
-	// set transport
-	fmt.Println("[main.info]Set Tr.Dial")
-
-	proxy.Tr.Dial = func(network, addr string) (c net.Conn, err error) {
-		fmt.Println("Dial to parent")
-		cipher, err := ss.NewCipher(config.ParentServer.Method, config.ParentServer.Key)
-		if err != nil {
-			proxy.Logger.Fatal("create shadowsocks cipher:", err)
-			return nil, err
-		}
-		c, err = dial(addr, config.ParentServer.HostAndPort, cipher.Copy(), 121)
-		if err != nil {
-			proxy.Logger.Fatalf("can't connect to shadowsocks parent %s for %s: %v\n",
-				config.ParentServer.HostAndPort, addr, err)
-			return nil, err
-		}
-		return
-	}
 
 func (pm *ProxydManager) add(uid uint64, listener net.Listener, conn net.Conn) {
 	pm.Lock()
