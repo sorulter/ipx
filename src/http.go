14e294b9f9a13905535f396b88b716e70c780259
Frank Kung <kfanjian@gmail.com>
2016-01-04 23:37:21 +0800

    Add HttpServer struct.

---
+++ src/http.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net"
+	"net/http"
+	"regexp"
+)
+
+type HttpServer struct {
+	Uid             uint64
+	Logger          *log.Logger
+	NotFoundHandler http.Handler
+	Tr              *http.Transport
+	ConnectDial     func(network string, addr string) (net.Conn, error)
+}
+
+var (
+	hasPort = regexp.MustCompile(`:\d+$`)
+)
