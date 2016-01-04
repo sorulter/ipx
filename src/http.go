2b6e6f2ebca1b697e6081d6277c6622209c8546e
Frank Kung <kfanjian@gmail.com>
2016-01-04 23:39:57 +0800

    New http server.

--- src/http.go
+++ src/http.go
@@ -20,4 +20,15 @@ var (
 )
 
 func (h *HttpServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+func NewHttpServer(uid uint64) *HttpServer {
+	return &HttpServer{
+		Uid: uid,
+		NotFoundHandler: http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			http.Error(w, "Illegal requests.", 500)
+		}),
+		Tr: &http.Transport{
+			Proxy: http.ProxyFromEnvironment},
+	}
+}
 }
