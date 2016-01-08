f83cb0e7eebd380fac343666aa0191a6e2a6da5e
Frank Kung <kfanjian@gmail.com>
2016-01-09 00:50:23 +0800

    Upload local flow data to remote mysql server.

--- src/counter.go
+++ src/counter.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/jinzhu/gorm"
 	"github.com/lessos/lessgo/data/hissdb"
 	"github.com/lessos/lessgo/logger"
 )
@@ -17,4 +18,22 @@ func counter(uid uint64, bytes int64) {
 	if rs := ssdb.Cmd("incr", key, fmt.Sprint(bytes)); rs.State != hissdb.ReplyOK {
 		logger.Printf("warn", "Log flow error: %v, incr %s %d\n", rs.State, key, bytes)
 	}
+}
+
+// Upload local flow data to remote mysql server.
+func upload() {
+	now := time.Now().In(loc)
+	before := now.Add(-6e10)
+
+	uplimit := now.Format("ipx.200601021504")
+	downlimit := before.Format("ipx.200601021504")
+
+	if rs := ssdb.Cmd("scan", downlimit, uplimit, 9999999); rs.State == hissdb.ReplyOK {
+		for _, v := range rs.Hash() {
+			db.Table("flows").Where("user_id = ?", v.Key[17:]).Update("used", gorm.Expr("used + ?", v.Value))
+		}
+	} else {
+		logger.Printf("warn", "[ssdb]cmd error: scan %s %s 9999999", downlimit, uplimit)
+	}
+
 }
