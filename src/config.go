463bceda4da4a61b22ca470115b536ed701bdd9e
Frank Kung <kfanjian@gmail.com>
2016-01-14 23:35:52 +0800

    Remove api server port config.

--- src/config.go
+++ src/config.go
@@ -1,8 +1,7 @@
 package main
 
 type Config struct {
-	ApiServerPort uint16 `json:"api_server_port"`
-	ParentServer  struct {
+	ParentServer struct {
 		HostAndPort string `json:"host_and_port"`
 		Method      string `json:"method"`
 		Key         string `json:"key"`
