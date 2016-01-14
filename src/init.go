e4be30b39389c54ce7a593df1be20b4544237c57
Frank Kung <kfanjian@gmail.com>
2016-01-14 21:44:28 +0800

    Modify log package.

--- src/init.go
+++ src/init.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"flag"
-	"os"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 	gconf "github.com/gocubes/config"
 	"github.com/jinzhu/gorm"
 	"github.com/lessos/lessgo/data/hissdb"
-	"github.com/lessos/lessgo/logger"
 )
 
 var (
@@ -21,14 +20,12 @@ func init() {
 
 	provier, perr := gconf.New("etc/config.json", "json")
 	if perr != nil {
-		logger.Printf("fatal", "[init]create config provider error: %v\n", perr.Error())
-		os.Exit(0)
+		log.Fatalf("[init]create config provider error: %v\n", perr.Error())
 	}
 
 	gerr := provier.Get(&config)
 	if gerr != nil {
-		logger.Printf("fatal", "[init]get config data error: %v\n", gerr.Error())
-		os.Exit(0)
+		log.Fatalf("[init]get config data error: %v\n", gerr.Error())
 	}
 
 	initDB()
@@ -43,13 +40,11 @@ func initDB() {
 		MaxConn: config.FlowCounter.SSDB.MaxConn,
 	})
 	if err != nil {
-		logger.Printf("fatal", "init ssdb error: %s", err.Error())
-		os.Exit(0)
+		log.Fatalf("[init db]Connect to ssdb error: %s", err.Error())
 	}
 
 	db, err = gorm.Open("mysql", config.FlowCounter.DB.DSN)
 	if err != nil {
-		logger.Printf("fatal", "MySQL Connect error: %v\n", err.Error())
-		os.Exit(0)
+		log.Fatalf("[init db]MySQL Connect error: %v\n", err.Error())
 	}
 }
